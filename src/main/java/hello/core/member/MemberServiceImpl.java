49b9311cbf504e82d688747bfff009081615da15
js.park <jsongeop@mediawill.com>
2023-01-31 14:12:47 +0900

    회원 도메인 실행과 테스트

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,6 +2,8 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
+    // MemberServiceImpl은 MemberRepository 와 MemoryMemberRepository 를 의존함
+    // 추상화/구체화에 의존하므로 DIP를 위반
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
